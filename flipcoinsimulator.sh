b2f1827f58979e1a3658fc350ea710cf92f76163
Aditya Gour <adityagour442@gmail.com>
2021-07-26 15:56:27 +0530

    Multiple

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -4,4 +4,18 @@ echo "Welcome to Flip Coin Simulation Program"
 head=0
 Hcount=0
 Tcount=0
-RandomCheck=$((RANDOM % 2))
+while [ True ] 
+do
+	randomCheck=$((RANDOM % 2))
+	if [ $randomCheck == 0 ]
+	then
+		echo "HEADS"
+		Hcount=$(($Hcount + 1))
+
+	else
+		echo "TAILS"
+		Tcount=$(($Tcount + 1))
+
+	fi
+done
+
